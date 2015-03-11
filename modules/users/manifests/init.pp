328272c0edab8ed6eb3ce57d8a14fc517ed4d9ed
Dave Try <davetry@Daves-MBP.home.gateway>
2015-03-12 09:07:01 +1100

    fixed sh to ssh

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -16,7 +16,7 @@ class users {
                 home => '/home/riley',
                 managehome => true,
                 }
-		sh_authorized_key { 'riley_ssh':
+		ssh_authorized_key { 'riley_ssh':
                 user => 'riley',
                 type => 'rsa',
                 key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDFEy3lgPQiaHjiI/9uKjdfLxrbP6p9lrJ8VX/d5pL3NIvdBM91Cv4az6fCuVhgh+CTuk4bvlHfKI6Cieu0eNOGjxEKUIliWkck5CSlVokYoqcqe7UZIUau/jwzpT70p2PSbypqdclhlkaBsuRmgh83VB6tdV6F5PAdStLFelqif5rFBJqcFGdBTLzmpw9FAoJez0PtceFhcY6lU75uzST2t58IDavTqg8odq6q/VpiQtbmOVaGMaEjmAY5FC0p3ZUThoBPERmGs0XSHb5cbSoyHqTQNWn6X0l43RDoS/lP3UGeoefVgGlH06WSoXCLaAhVR1I0qSsqpeqyQ5AKVLtN',
