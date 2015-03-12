84c024522742054ade4fde77f4eb97a8308923bd
Dave Try <davetry@Daves-MBP.home.gateway>
2015-03-12 13:07:17 +1100

    added user for cam

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -32,5 +32,16 @@ class users {
                 type => 'rsa',
                 key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDFEy3lgPQiaHjiI/9uKjdfLxrbP6p9lrJ8VX/d5pL3NIvdBM91Cv4az6fCuVhgh+CTuk4bvlHfKI6Cieu0eNOGjxEKUIliWkck5CSlVokYoqcqe7UZIUau/jwzpT70p2PSbypqdclhlkaBsuRmgh83VB6tdV6F5PAdStLFelqif5rFBJqcFGdBTLzmpw9FAoJez0PtceFhcY6lU75uzST2t58IDavTqg8odq6q/VpiQtbmOVaGMaEjmAY5FC0p3ZUThoBPERmGs0XSHb5cbSoyHqTQNWn6X0l43RDoS/lP3UGeoefVgGlH06WSoXCLaAhVR1I0qSsqpeqyQ5AKVLtN',
                 }
+	user { 'cam':
+                ensure => present,
+                comment => 'Cam Lee',
+                home => '/home/cam',
+                managehome => true,
+                }
+                ssh_authorized_key { 'cam_ssh':
+                user => 'cam',
+                type => 'rsa',
+                key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDFEy3lgPQiaHjiI/9uKjdfLxrbP6p9lrJ8VX/d5pL3NIvdBM91Cv4az6fCuVhgh+CTuk4bvlHfKI6Cieu0eNOGjxEKUIliWkck5CSlVokYoqcqe7UZIUau/jwzpT70p2PSbypqdclhlkaBsuRmgh83VB6tdV6F5PAdStLFelqif5rFBJqcFGdBTLzmpw9FAoJez0PtceFhcY6lU75uzST2t58IDavTqg8odq6q/VpiQtbmOVaGMaEjmAY5FC0p3ZUThoBPERmGs0XSHb5cbSoyHqTQNWn6X0l43RDoS/lP3UGeoefVgGlH06WSoXCLaAhVR1I0qSsqpeqyQ5AKVLtN',
+                }
 	}	
 	
