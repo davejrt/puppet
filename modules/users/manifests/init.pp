72578dd8e7a97ed8b62b1770e2ae14cee01b3952
Dave Try <davetry@Daves-MBP.home.gateway>
2015-03-12 12:39:16 +1100

    trying to fix the permissions issue

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -27,10 +27,10 @@ class users {
                 home => '/home/rich',
                 managehome => true,
                 }
-                ssh_authorized_key { 'rich_ssh':
-                user => 'rich',
-                type => 'rsa',
-                key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDFEy3lgPQiaHjiI/9uKjdfLxrbP6p9lrJ8VX/d5pL3NIvdBM91Cv4az6fCuVhgh+CTuk4bvlHfKI6Cieu0eNOGjxEKUIliWkck5CSlVokYoqcqe7UZIUau/jwzpT70p2PSbypqdclhlkaBsuRmgh83VB6tdV6F5PAdStLFelqif5rFBJqcFGdBTLzmpw9FAoJez0PtceFhcY6lU75uzST2t58IDavTqg8odq6q/VpiQtbmOVaGMaEjmAY5FC0p3ZUThoBPERmGs0XSHb5cbSoyHqTQNWn6X0l43RDoS/lP3UGeoefVgGlH06WSoXCLaAhVR1I0qSsqpeqyQ5AKVLtN',
-                }
+#                ssh_authorized_key { 'rich_ssh':
+#                user => 'rich',
+#                type => 'rsa',
+#                key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDFEy3lgPQiaHjiI/9uKjdfLxrbP6p9lrJ8VX/d5pL3NIvdBM91Cv4az6fCuVhgh+CTuk4bvlHfKI6Cieu0eNOGjxEKUIliWkck5CSlVokYoqcqe7UZIUau/jwzpT70p2PSbypqdclhlkaBsuRmgh83VB6tdV6F5PAdStLFelqif5rFBJqcFGdBTLzmpw9FAoJez0PtceFhcY6lU75uzST2t58IDavTqg8odq6q/VpiQtbmOVaGMaEjmAY5FC0p3ZUThoBPERmGs0XSHb5cbSoyHqTQNWn6X0l43RDoS/lP3UGeoefVgGlH06WSoXCLaAhVR1I0qSsqpeqyQ5AKVLtN',
+#                }
 	}	
 	
