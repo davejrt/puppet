856f84197eb05122d9d97cc1422448f194e8b2c9
Dave Try <davetry@Daves-MacBook-Pro.local>
2015-04-02 22:53:51 +1100

    code

--- modules/splunk/manifests/init.pp
+++ modules/splunk/manifests/init.pp
@@ -3,6 +3,7 @@ class splunk {
 		package { 'splunkforwarder'
 		provider => 'rpm',
 		source => 'puppet:///modules/splunk/splunkforwarder-6.2.1-245427-linux-2.6-x86_64.rpm'
+		ensure => installed,
 		
 		
 		}
