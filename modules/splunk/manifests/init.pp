0be64b4bac986314912378ed89c799ffe2765bd9
Dave Try <davetry@Daves-MBP.home.gateway>
2015-04-06 19:44:28 +1000

    fixed syntax

--- modules/splunk/manifests/init.pp
+++ modules/splunk/manifests/init.pp
@@ -1,35 +1,35 @@
 class splunk {
 
 		package { 'splunkforwarder':
-		provider => 'rpm',
-		source => '/tmp/splunkforwarder-6.2.1-245427-linux-2.6-x86_64.rpm',
-		install_options => '-ivh',
-		ensure => installed,
-		require => File["/tmp/splunkforwarder-6.2.1-245427-linux-2.6-x86_64.rpm"],
+			provider => 'rpm',
+			source => '/tmp/splunkforwarder-6.2.1-245427-linux-2.6-x86_64.rpm',
+			install_options => '-ivh',
+			ensure => installed,
+			require => File["/tmp/splunkforwarder-6.2.1-245427-linux-2.6-x86_64.rpm"],
 		
 		
 		}
 
-	file {'/tmp/splunkforwarder-6.2.1-245427-linux-2.6-x86_64.rpm':
-		source => 'puppet:///modules/splunk/splunkforwarder-6.2.1-245427-linux-2.6-x86_64.rpm',
-		ensure => present,
+	    file {'/tmp/splunkforwarder-6.2.1-245427-linux-2.6-x86_64.rpm':
+			source => 'puppet:///modules/splunk/splunkforwarder-6.2.1-245427-linux-2.6-x86_64.rpm',
+			ensure => present,
 		}
 		
-	file {'/opt/splunkforwarder/etc/apps/search/local/inputs.conf':
-		source => 'puppet:///modules/splunk/inputs.conf',
-		ensure => present,
-		owner => 'splunk',
-		group => 'splunk',
-		mode => '0600',
+	    file {'/opt/splunkforwarder/etc/apps/search/local/inputs.conf':
+			source => 'puppet:///modules/splunk/inputs.conf',
+			ensure => present,
+			owner => 'splunk',
+			group => 'splunk',
+			mode => '0600',
 		}
 
-	file {'/opt/splunkforwarder/etc/apps/search/metadata/local.meta':
-                source => 'puppet:///modules/splunk/local.meta',
-                ensure => present,
-                owner => 'splunk',
-                group => 'splunk',
-                mode => '0600',
-                }`
+	    file {'/opt/splunkforwarder/etc/apps/search/metadata/local.meta':
+            source => 'puppet:///modules/splunk/local.meta',
+            ensure => present,
+            owner => 'splunk',
+	        group => 'splunk',
+            mode => '0600',
+		}
 
 
 }
