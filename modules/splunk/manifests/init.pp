7718123615e00d134f19a90b5113aaf20f951fb7
Dave Try <davetry@Daves-MBP.home.gateway>
2015-04-06 19:39:53 +1000

    code

--- modules/splunk/manifests/init.pp
+++ modules/splunk/manifests/init.pp
@@ -10,10 +10,28 @@ class splunk {
 		
 		}
 
-		file {'/tmp/splunkforwarder-6.2.1-245427-linux-2.6-x86_64.rpm':
+	file {'/tmp/splunkforwarder-6.2.1-245427-linux-2.6-x86_64.rpm':
 		source => 'puppet:///modules/splunk/splunkforwarder-6.2.1-245427-linux-2.6-x86_64.rpm',
 		ensure => present,
 		}
+		
+	file {'/opt/splunkforwarder/etc/apps/search/local/inputs.conf':
+		source => 'puppet:///modules/splunk/inputs.conf',
+		ensure => present,
+		owner => 'splunk',
+		group => 'splunk',
+		mode => '0600',
+		}
+
+	file {'/opt/splunkforwarder/etc/apps/search/metadata/local.meta':
+                source => 'puppet:///modules/splunk/local.meta',
+                ensure => present,
+                owner => 'splunk',
+                group => 'splunk',
+                mode => '0600',
+                }`
+
+
 }
 
 	
